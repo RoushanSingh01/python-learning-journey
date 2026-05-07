72678c59bcd37ed4fec26bebde0327ece0a977b2
Roushan Singh <thisisroushan01@gmail.com>
2026-05-07 18:24:22 +0530

    Refactor variable names and add local test cases

--- leetcode/medium/033_search_in_rotated_sorted_array.py
+++ leetcode/medium/033_search_in_rotated_sorted_array.py
@@ -1,14 +1,23 @@
 class Solution:
     def search(self, nums, target):
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r) // 2
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
             if nums[mid] == target:
                 return mid
-            elif sum((target < nums[l], nums[l] <= nums[mid], nums[mid] < target)) == 2:
-                l = mid + 1
+
+            elif sum((
+                target < nums[left],
+                nums[left] <= nums[mid],
+                nums[mid] < target
+            )) == 2:
+                left = mid + 1
+
             else:
-                r = mid - 1
+                right = mid - 1
+
         return -1
 
 
@@ -27,5 +36,3 @@ if __name__ == "__main__":
     run_test([3, 1], 1, 1)
     run_test([5, 1, 3], 5, 0)
     run_test([5, 1, 3], 3, 2)
-    run_test([6, 7, 8, 1, 2, 3, 4, 5], 2, 4)
-    run_test([6, 7, 8, 1, 2, 3, 4, 5], 9, -1)
