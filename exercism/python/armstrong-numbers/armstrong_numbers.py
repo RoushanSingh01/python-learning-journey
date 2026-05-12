82373b7932b6949ef18da54efe753f2ff3def1a6
Roushan Singh <thisisroushan01@gmail.com>
2026-05-12 17:59:26 +0530

    Improve Armstrong number implementation to satisfy Pylint standards

--- exercism/python/armstrong-numbers/armstrong_numbers.py
+++ exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,4 +1,8 @@
 def is_armstrong_number(number):
-    digits = str(number)
-    power = len(digits)
-    return sum(int(digit) ** power for digit in digits) == number
+    digit_string = str(number)
+    digit_count = len(digit_string)
+
+    return (
+        sum(int(digit) ** digit_count for digit in digit_string)
+        == number
+    )
