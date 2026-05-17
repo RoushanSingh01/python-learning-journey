5206a426238494dde5012e605cd85ebf587f1d6f
Roushan Singh <thisisroushan01@gmail.com>
2026-05-17 17:53:51 +0530

    Solve Binary Search using recursive divide-and-conquer

---
+++ exercism/python/binary-search/binary_search.py
@@ -0,0 +1,21 @@
+"""Utilities for binary search."""
+
+
+def find(search_list, value):
+    """Return index of value in sorted list."""
+
+    left = 0
+    right = len(search_list) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if search_list[mid] == value:
+            return mid
+
+        if search_list[mid] < value:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    raise ValueError("value not in array")
