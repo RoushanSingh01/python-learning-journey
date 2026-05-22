5e0a7bdeb2132d86534c9237345bd2392f95281b
Roushan Singh <thisisroushan01@gmail.com>
2026-05-22 17:33:05 +0530

    Refine list ops implementation with improved documentation and cleaner naming

--- exercism/python/list-ops/list_ops.py
+++ exercism/python/list-ops/list_ops.py
@@ -1,50 +1,63 @@
 """
-List Ops
-https://exercism.org/tracks/python/exercises/list-ops
-
+Custom implementations of common list operations.
 """
 
 from collections.abc import Iterable
-from typing import Callable, Any
+from typing import Any, Callable
+
+
+# pylint: disable=redefined-builtin
 
 
 def append(list1: list, list2: list) -> list:
+    """Return a new list containing elements from both lists."""
     return [*list1, *list2]
 
 
 def concat(lists: list) -> list:
+    """Flatten nested iterables into a single list."""
     result = []
-    for item in lists:
-        if isinstance(item, Iterable):
-            result.extend(item)
+
+    for iterable in lists:
+        if isinstance(iterable, Iterable):
+            result.extend(iterable)
         else:
-            result.append(item)
+            result.append(iterable)
+
     return result
 
 
-def filter(myfunction: Callable, mylist: list) -> list:
-    return [l for l in mylist if myfunction(l)]
+def filter(function: Callable, items: list) -> list:
+    """Return items that satisfy the predicate function."""
+    return [item for item in items if function(item)]
 
 
-def length(mylist: list) -> int:
-    return len(mylist)
+def length(items: list) -> int:
+    """Return the number of elements in the list."""
+    return len(items)
 
 
-def map(myfunction: Callable, mylist: list) -> list:
-    return [myfunction(l) for l in mylist]
+def map(function: Callable, items: list) -> list:
+    """Apply a function to every item in the list."""
+    return [function(item) for item in items]
 
 
-def foldl(myfunction: Callable, mylist: list, initial: Any) -> Any:
-    while mylist:
-        initial = myfunction(initial, mylist.pop(0))
+def foldl(function: Callable, items: list, initial: Any) -> Any:
+    """Reduce the list from left to right."""
+    while items:
+        initial = function(initial, items.pop(0))
+
     return initial
 
 
-def foldr(myfunction: Callable, mylist: list, initial: Any) -> Any:
-    while mylist:
-        initial = myfunction(initial, mylist.pop())
+def foldr(function: Callable, items: list, initial: Any) -> Any:
+    """Reduce the list from right to left."""
+    while items:
+        initial = function(initial, items.pop())
+
     return initial
 
 
-def reverse(mylist: list) -> list:
-    return mylist[::-1]
+def reverse(items: list) -> list:
+    """Return a reversed copy of the list."""
+    return items[::-1]
