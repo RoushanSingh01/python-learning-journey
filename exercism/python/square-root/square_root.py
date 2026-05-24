8da742a9451ddb1a33ebffa20d93beabf310db9e
Roushan Singh <thisisroushan01@gmail.com>
2026-05-24 16:07:44 +0530

    feat: solve square root using linear search

---
+++ exercism/python/square-root/square_root.py
@@ -0,0 +1,19 @@
+"""Compute the integer square root of a perfect square."""
+
+
+def square_root(number):
+    """Return the square root of the given perfect square."""
+
+    left, right = 0, number
+
+    while left <= right:
+        mid = (left + right) // 2
+        square = mid * mid
+
+        if square == number:
+            return mid
+
+        if square < number:
+            left = mid + 1
+        else:
+            right = mid - 1
