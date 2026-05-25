3e405ce415f51461daefd9295c4846ece34bb65f
Roushan Singh <thisisroushan01@gmail.com>
2026-05-25 17:31:50 +0530

    feat: solve find minimum in rotated sorted array using built-in min

---
+++ leetcode/medium/153_find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,12 @@
+class Solution:
+    findMin = min
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    nums = [3, 4, 5, 1, 2]
+
+    result = solution.findMin(nums)
+
+    print(result)
