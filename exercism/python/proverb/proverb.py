2a5c7ed8fb7eda572e5e4c902d9a66285ea783a5
Roushan Singh <thisisroushan01@gmail.com>
2026-05-31 17:47:32 +0530

    solve: implement proverb generator

---
+++ exercism/python/proverb/proverb.py
@@ -0,0 +1,19 @@
+def proverb(*lst, qualifier=None):
+    result = []
+
+    for first, second in zip(lst, lst[1:]):
+        result.append(
+            f"For want of a {first} the {second} was lost."
+        )
+
+    if lst:
+        if qualifier is not None:
+            result.append(
+                f"And all for the want of a {qualifier} {lst[0]}."
+            )
+        else:
+            result.append(
+                f"And all for the want of a {lst[0]}."
+            )
+
+    return result
