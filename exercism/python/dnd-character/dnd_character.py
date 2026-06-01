90245a244367f1248eb27ac4f9acb7188f1915d9
Roushan Singh <thisisroushan01@gmail.com>
2026-06-01 15:51:06 +0530

    solve: generate dnd character using ability rolls and dynamic attribute assignment

---
+++ exercism/python/dnd-character/dnd_character.py
@@ -0,0 +1,32 @@
+"""Generate a D&D character and ability scores."""
+
+from random import randint
+
+
+def modifier(score):
+    """Calculate the ability modifier."""
+    return (score - 10) // 2
+
+
+class Character:
+    """Represents a D&D character."""
+
+    def __init__(self):
+        abilities = (
+            "strength",
+            "dexterity",
+            "constitution",
+            "intelligence",
+            "wisdom",
+            "charisma",
+        )
+
+        for ability in abilities:
+            setattr(self, ability, self.ability())
+
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    @staticmethod
+    def ability():
+        """Generate an ability score."""
+        return sum(sorted(randint(1, 6) for _ in range(4))[1:])
