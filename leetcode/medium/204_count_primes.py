89353d75edbf10f92f6fe4360d12b111a4f2bcc1
Roushan Singh <thisisroushan01@gmail.com>
2026-06-03 16:16:59 +0530

    Optimize prime counting using Sieve of Eratosthenes

--- leetcode/medium/204_count_primes.py
+++ leetcode/medium/204_count_primes.py
@@ -1,26 +1,39 @@
 class Solution:
-    def countPrimes(self, n):
+    def countPrimes(self, n: int) -> int:
         if n < 3:
             return 0
 
-        prime = [True] * n
-        prime[0] = prime[1] = False
+        sieve = [True] * n
+        sieve[0] = sieve[1] = False
 
-        for i in range(2, int(n ** 0.5) + 1):
-            if prime[i]:
-                prime[i * i:n:i] = [False] * len(
-                    prime[i * i:n:i]
+        limit = int(n ** 0.5) + 1
+
+        for i in range(3, limit, 2):
+            if sieve[i]:
+                sieve[i * i:n:2 * i] = [False] * len(
+                    sieve[i * i:n:2 * i]
                 )
 
-        return sum(prime)
+        count = 1  # Prime number 2
+
+        for i in range(3, n, 2):
+            if sieve[i]:
+                count += 1
+
+        return count
 
 
 if __name__ == "__main__":
     sol = Solution()
 
-    test_cases = [10, 0, 1, 2, 100]
+    test_cases = [
+        0,
+        1,
+        2,
+        10,
+        100,
+        1000,
+    ]
 
     for n in test_cases:
-        print(f"n = {n}")
-        print("count =", sol.countPrimes(n))
-        print()
+        print(f"Primes less than {n}: {sol.countPrimes(n)}")
