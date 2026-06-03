1c4adbd74323f5c70e4a5d0c27b2fb8fd6426618
Roushan Singh <thisisroushan01@gmail.com>
2026-06-03 15:45:06 +0530

    Refactor transpose solution to satisfy pylint recommendations

--- exercism/python/transpose/transpose.py
+++ exercism/python/transpose/transpose.py
@@ -5,21 +5,17 @@ def transpose(text):
     """Return the transposed representation of a multiline string."""
     lines = text.split("\n")
 
-    max_length = len(lines[0])
-    for line in lines:
-        if len(line) > max_length:
-            max_length = len(line)
+    max_length = max(len(line) for line in lines)
 
-    result = ["" for _ in range(max_length)]
+    result = [""] * max_length
 
     remaining_lines = len(lines)
 
     for line in lines:
         remaining_lines -= 1
-        length = len(line)
 
         for index in range(max_length):
-            if index >= length:
+            if index >= len(line):
                 if remaining_lines:
                     result[index] += " "
                 continue
@@ -27,7 +23,7 @@ def transpose(text):
             result[index] += line[index]
 
     for index in range(max_length - 1, -1, -1):
-        if result[index][-1] != " ":
+        if not result[index].endswith(" "):
             break
 
         result[index] = result[index].rstrip()
