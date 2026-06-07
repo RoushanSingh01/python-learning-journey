7ba489a9d3dc4545e58ec60889c2d8e77a0d32d0
Roushan Singh <thisisroushan01@gmail.com>
2026-06-07 18:56:25 +0530

    Solve simple cipher using character shift mapping

---
+++ exercism/python/simple-cipher/simple_cipher.py
@@ -0,0 +1,40 @@
+"""Simple substitution cipher."""
+
+from string import ascii_lowercase
+
+
+class Cipher:
+    """Encode and decode messages."""
+
+    def __init__(self, key="aaaaaaaaaa"):
+        """Store the cipher key."""
+        self.key = key
+
+    def encode(self, text):
+        """Encode text."""
+        return self._transform(text, 1)
+
+    def decode(self, text):
+        """Decode text."""
+        return self._transform(text, -1)
+
+    def _transform(self, text, direction):
+        """Apply cipher transformation."""
+        result = []
+
+        for index, character in enumerate(text):
+            text_value = ord(character) - ord("a")
+            key_value = ord(
+                self.key[index % len(self.key)]
+            ) - ord("a")
+
+            shifted = (
+                text_value
+                + direction * key_value
+            ) % 26
+
+            result.append(
+                chr(shifted + ord("a"))
+            )
+
+        return "".join(result)
