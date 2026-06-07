5f13c374857c9b4fba6720ff5a68c00813bcec18
Roushan Singh <thisisroushan01@gmail.com>
2026-06-07 18:45:05 +0530

    Solve Contains Duplicate using set lookup

---
+++ leetcode/easy/217_contains_duplicate.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+if __name__ == "__main__":
+    print(Solution().containsDuplicate([1, 2, 3, 1]))
+    print(Solution().containsDuplicate([1, 2, 3, 4]))
+    print(Solution().containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
