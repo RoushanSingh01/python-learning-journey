93d5374a231cf2ca0eb5a462a848cb2cca12e438
Roushan Singh <thisisroushan01@gmail.com>
2026-06-09 16:15:01 +0530

    Solve simple-linked-list using singly linked nodes

---
+++ exercism/python/simple-linked-list/simple_linked_list.py
@@ -0,0 +1,70 @@
+"""Simple linked list implementation."""
+
+
+class Node:
+    """Node in a linked list."""
+
+    def __init__(self, value):
+        self._value = value
+        self._next = None
+
+    def value(self):
+        return self._value
+
+    def next(self):
+        return self._next
+
+
+class LinkedList:
+    """Singly linked list."""
+
+    def __init__(self, values=None):
+        self._head = None
+
+        if values is not None:
+            for value in values:
+                self.push(value)
+
+    def __len__(self):
+        count = 0
+        current_node = self._head
+
+        while current_node is not None:
+            count += 1
+            current_node = current_node.next()
+
+        return count
+
+    def head(self):
+        if self._head is None:
+            raise EmptyListException("The list is empty.")
+
+        return self._head
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node._next = self._head
+        self._head = new_node
+
+    def pop(self):
+        current_node = self.head()
+        value = current_node.value()
+        self._head = current_node.next()
+        return value
+
+    def __iter__(self):
+        current_node = self._head
+
+        while current_node is not None:
+            yield current_node.value()
+            current_node = current_node.next()
+
+    def reversed(self):
+        return LinkedList(list(self))
+
+
+class EmptyListException(Exception):
+    """Raised when operating on an empty list."""
+
+    def __init__(self, message):
+        super().__init__(message)
