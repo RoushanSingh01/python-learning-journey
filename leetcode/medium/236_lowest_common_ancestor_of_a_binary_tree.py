464b2a87c7dc4a12602d1c8b20d824e4470a7288
Roushan Singh <thisisroushan01@gmail.com>
2026-06-12 16:49:18 +0530

    Solve Lowest Common Ancestor of a Binary Tree using recursive DFS

---
+++ leetcode/medium/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if root in (None, p, q):
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        return root if left and right else (left or right)
+
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(5)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(6)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(0)
+    root.right.right = TreeNode(8)
+    root.left.right.left = TreeNode(7)
+    root.left.right.right = TreeNode(4)
+
+    p = root.left
+    q = root.left.right.right
+
+    print(Solution().lowestCommonAncestor(root, p, q).val)
