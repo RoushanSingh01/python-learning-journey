55052bc268bc8036ef8b9f742a2c33cf05fe116c
Roushan Singh <thisisroushan01@gmail.com>
2026-06-12 16:44:31 +0530

    Solve Lowest Common Ancestor of a BST using BST property traversal

---
+++ leetcode/medium/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                return root
+
+
+if __name__ == "__main__":
+    root = TreeNode(6)
+    root.left = TreeNode(2)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(4)
+    root.left.right.left = TreeNode(3)
+    root.left.right.right = TreeNode(5)
+    root.right.left = TreeNode(7)
+    root.right.right = TreeNode(9)
+
+    p = root.left
+    q = root.left.right
+
+    print(Solution().lowestCommonAncestor(root, p, q).val)
