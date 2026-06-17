297e887c7029f428e26956809874bc4d9f2f47a1
Roushan Singh <thisisroushan01@gmail.com>
2026-06-17 16:32:16 +0530

    Solve Find the Duplicate Number using Floyd cycle detection

---
+++ leetcode/medium/287_find_the_duplicate_number.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test_cases = [
+        [1, 3, 4, 2, 2],
+        [3, 1, 3, 4, 2],
+        [1, 1],
+        [1, 1, 2],
+    ]
+
+    for nums in test_cases:
+        print(f"Input : {nums}")
+        print(f"Output: {solution.findDuplicate(nums)}")
+        print()
